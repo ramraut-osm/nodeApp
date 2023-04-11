1be7b03099985888118b58b91471708ea617749b
Ram <ram.raut@osmosys.asia>
2023-04-11 13:56:22 +0530

    add user function

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use(bodyparser.urlencoded({ extended: true }))
 
 // set view engine
 app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
---
+++ server/routes/router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+/**
+ *  @description Root Route
+ *  @method GET /
+ */
+route.get('/', services.homeRoutes);
+
+/**
+ *  @description add users
+ *  @method GET /add-user
+ */
+route.get('/add-user', services.add_user)
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+
+module.exports = route
